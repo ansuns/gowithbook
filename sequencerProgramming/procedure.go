cd9ed4a76d4131abc7253e202e6354482268e9ad
ansuns <25102483@qq.com>
2019-02-17 14:44:55 +0800

    流程控制

---
+++ sequencerProgramming/procedure.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+/**
+流程控制
+ */
+
+func main() {
+	val := first(7)
+	fmt.Println(val)
+
+	i := 1
+	switch i {
+	case 0:
+		fmt.Println(0)
+	case 1:
+		fmt.Println(1)
+	case 2:
+		fmt.Println(2)
+	default:
+		fmt.Println("Default")
+
+	}
+
+	//switch后面的表达式可能也不是必须的：
+	switch {
+	case 0 < val:
+		fmt.Println("aa")
+	default:
+		fmt.Println("not aa")
+
+	}
+}
+
+func first(a int) int {
+	if a == 0 {
+		return 4
+	} else {
+		return a * a
+	}
+}

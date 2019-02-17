e7df7edfc97e2d1054b9e817928ef71625faf9e6
ansuns <25102483@qq.com>
2019-02-17 19:08:11 +0800

    modifed

--- objectOrientedProgramming/typesystem.go
+++ objectOrientedProgramming/typesystem.go
@@ -2,17 +2,18 @@ package main
 
 import "fmt"
 
-type Interger int //定义一个新的类型Interger
+type Interger int //定义一个新的类型Interger(使用 type 关键字可以定义出新的自定义类型)
 
 /**
-增加一个Less方法，是Interger的内部方法
+给Interger类型添加 Less 方法，int原来的方法被Integer继承
+增加一个Less方法
  */
 func (a Interger) Less(b Interger) bool {
 	return a < b
 }
 
 func main() {
-	var a Interger = 1
+	var a Interger = 1 //a 是一个int对象
 	if a.Less(2) {
 		fmt.Println(a, "less 2")
 	}

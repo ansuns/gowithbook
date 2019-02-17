38cf0f3f9b054b4b3b859c1d72b276903ee035d6
ansuns <25102483@qq.com>
2019-02-17 19:49:47 +0800

    结构体：匿名组合

--- objectOrientedProgramming/struct.go
+++ objectOrientedProgramming/struct.go
@@ -17,6 +17,33 @@ func (r *Rect) Area() float64 {
 	return r.width * r.height
 }
 
+//提供继承，但是已组合的文法（匿名组合）
+//定义一个Base类，实现Foo(),Bar()两个成员方法
+type Base struct {
+	Name string
+}
+
+//为Base类增加Foo方法
+func (base *Base) Foo() {
+
+}
+
+//为Base累增加Bar方法
+func (base *Base) Bar() {
+
+}
+
+//定义Foo类，从Base继承
+type Foo struct {
+	Base
+	//...其他成员
+}
+
+//改写Bar方法
+func (foo *Foo) Bar() {
+	foo.Base.Bar()
+}
+
 func main() {
 	//创建Rect类型对象实例(1-4种方法都可以)
 	rect1 := new(Rect)

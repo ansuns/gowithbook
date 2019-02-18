b0a2aa41f725a0a699ed2fcd77098ce5dec5c513
ansuns <25102483@qq.com>
2019-02-18 22:30:57 +0800

    接口1

---
+++ objectOrientedProgramming/interface/interface.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+//定义一个接口
+type Human interface {
+	Say() //只有，不实现（用其他类实现）
+}
+
+//定义一个类
+type Student struct {
+}
+
+//实现Human方法
+func (s *Student) Say() {
+	fmt.Println("i‘m astudent")
+}
+
+//定义一个类
+type Teacher struct {
+}
+
+//实现Human方法
+func (t *Teacher) Say() {
+	fmt.Println("i‘m teacher")
+}
+func main() {
+	//已经有两个类型Student,Teacher实现了Human接口
+	var h Human
+	s := &Student{}
+	h = s
+	h.Say()
+
+	t := &Teacher{}
+	h = t
+	t.Say()
+
+}

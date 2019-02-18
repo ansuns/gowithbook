774800108ea09d60135f43a8c438a52421c7bc51
ansuns <25102483@qq.com>
2019-02-18 22:35:56 +0800

    接口1

--- objectOrientedProgramming/interface/interface.go
+++ objectOrientedProgramming/interface/interface.go
@@ -4,7 +4,7 @@ import "fmt"
 
 //定义一个接口
 type Human interface {
-	Say() //只有，不实现（用其他类实现）
+	Say() //只有声明，不实现（用其他类实现）
 }
 
 //定义一个类
@@ -25,7 +25,7 @@ func (t *Teacher) Say() {
 	fmt.Println("i‘m teacher")
 }
 func main() {
-	//已经有两个类型Student,Teacher实现了Human接口
+	//已经有两个类型Student,Teacher实现了Human接口，一个接口的不同提现
 	var h Human
 	s := &Student{}
 	h = s

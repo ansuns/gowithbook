90dcfe952d95e10e6cc35f465196c0cdad3be540
ansuns <25102483@qq.com>
2019-02-23 17:30:27 +0800

    通过channerl实现同步

---
+++ concurrentProgramming/2_channel.go
@@ -0,0 +1,36 @@
+package main
+
+//通过channerl 实现同步
+import (
+	"fmt"
+	"time"
+)
+
+//全局变量channerl
+var ch = make(chan int)
+
+func main() {
+	go person1()
+	go person2()
+	//主协程10s后结束
+	time.Sleep(time.Second * 10)
+}
+
+func Printer(str string) {
+	for _, data := range str {
+		fmt.Printf("%c", data)
+		time.Sleep(time.Second)
+	}
+	fmt.Printf("\n")
+}
+
+//person1执行完才执行person2
+func person1() {
+	Printer("Hello")
+	ch <- 1 //给管道写数据
+}
+
+func person2() {
+	<-ch //从管道去数据,接受，如果管道没有数据，则堵塞
+	Printer("World")
+}

5bbdb52c9854ec0cef0b6f323d66e3dbcf9909c7
ansuns <25102483@qq.com>
2019-02-23 10:35:35 +0800

    fix

--- helloWorld/hello.go
+++ helloWorld/hello.go
@@ -6,6 +6,8 @@ func main() {
 	fmt.Println("Hello World!")
 	_, _, nickname := getName()
 	fmt.Println(nickname)
+	info, err := orderInfo(158)
+	fmt.Println(info, err)
 }
 
 /*
@@ -15,5 +17,12 @@ func getName() (firstName, secondName, nickName string) {
 	firstName = "张"
 	secondName = "三"
 	nickName = "二狗"
+	return
+}
+
+/**
+没有明确赋值时，返回默认值，并不是所遇的返回值都必须赋值
+ */
+func orderInfo(order_id int) (info []int, err error) {
 	return
 }

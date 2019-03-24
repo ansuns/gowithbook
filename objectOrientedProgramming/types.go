1120f6208c9af96b298327cd9bc1253e757526f4
ansuns <251024883@qq.com>
2019-03-24 13:58:53 +0800

    fix

--- objectOrientedProgramming/types.go
+++ objectOrientedProgramming/types.go
@@ -12,9 +12,16 @@ func (a Interger) Less(b Interger) bool {
 	return a < b
 }
 
+func (a *Interger) Add(b Interger) {
+	*a += b
+}
+
 func main() {
 	var a Interger = 1 //a 是一个int对象
 	if a.Less(2) {
 		fmt.Println(a, "less 2")
 	}
+
+	a.Add(2)
+	fmt.Println(a)
 }

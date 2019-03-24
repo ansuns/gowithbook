52c76cccbabc0254ac432734b954e6b2d013b446
ansuns <251024883@qq.com>
2019-03-24 14:17:36 +0800

    匿名组合（继承）

---
+++ objectOrientedProgramming/onemoreStruct.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type Recta struct {
+	x, y          float64
+	width, height float64
+}
+
+func (r *Recta) Area() float64 {
+	return r.height * r.width
+}
+
+func main() {
+	//r := new(Recta)
+	r := &Recta{0,0,4,9}
+	res := r.Area()
+	fmt.Println(res)
+}
